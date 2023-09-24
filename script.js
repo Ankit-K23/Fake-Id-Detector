83b37ebc528e7ff9195cfbdd965aaca1257c3ced
Ankit <88825991+Ankit-K23@users.noreply.github.com>
2023-09-24 17:26:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function isfakeacc(user) {
 
-    if(user.followers > 1000000){
+    if(user.followersCount > 1000000){
         return true;
     }
 
-    if(user.postcount === 0){
+    if(user.postsCount === 0){
         return true;
     }
 
     if(!user.isverified && !user.profilepic){
+        return true;
+    }
+
+    const currentDate = new Date();
+    const creationDate = new Date(user.creationDate);
+    const accountAgeInDays = Math.floor((currentDate - creationDate) / (1000 * 60 * 60 * 24));
+
+    if (accountAgeInDays < 10) {
         return true;
     }
 
@@ -19,17 +27,67 @@ function isfakeacc(user) {
 const users = [
     {
         username: "ankit._.atcc",
-        followers: "80",
-        postcount: "2",
+        followersCount: "88",
+        postsCount: "0",
         isverified: false,
-        profilepic: "https://example.com/user1.jpg",
+        profilepic: "Avatar.png",
+        creationDate: '2021-12-01',
     },
     {
-        username: 'rohit',
-        followersCount: 50000,
-        postsCount: 0,
-        isVerified: true,
+        username: 'strange_gamer_yt_',
+        followersCount: 1314,
+        postsCount: 4,
+        isVerified: false,
         profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2020-07-01',
+    },
+    {
+        username: 'shubham_bhatti_11',
+        followersCount: 436,
+        postsCount: 39,
+        isVerified: false,
+        profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2020-07-01',
+    },
+    {
+        username: 'ig._.panda24',
+        followersCount: 25,
+        postsCount: 1,
+        isVerified: false,
+        profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2022-10-01',
+    },
+    {
+        username: 'krish_prajapati_07_',
+        followersCount: 275,
+        postsCount: 2,
+        isVerified: false,
+        profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2020-12-01',
+    },
+    {
+        username: 'harshvyas12324',
+        followersCount: 134,
+        postsCount: 3,
+        isVerified: false,
+        profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2021-11-01',
+    },
+    {
+        username: 'chandni._.05',
+        followersCount: 101,
+        postsCount: 1,
+        isVerified: false,
+        profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2019-08-01',
+    },
+    {
+        username: 'sltietrajkot',
+        followersCount: 1373,
+        postsCount: 491,
+        isVerified: false,
+        profilePicture: 'https://example.com/user2.jpg',
+        creationDate: '2018-10-01',
     },
 ];
 
@@ -44,8 +102,20 @@ function detect() {
         } else {
             result.innerHTML = `user ${useridinput} appears legitimate.`;
         }
+
+        document.getElementById('username').textContent = user.username;
+        document.getElementById('followersCount').textContent = user.followersCount;
+        document.getElementById('postsCount').textContent = user.postsCount;
+        document.getElementById('isVerified').textContent = user.isVerified ? 'Yes' : 'No';
+        document.getElementById('profilePicture').src = user.profilePicture;
+        document.getElementById('creationDate').textContent = user.creationDate;
+
+        userDetailsContainer.style.display = 'block';
+        boxx.style.display = 'flex';
+
     } else {
-        result.innerHTML = `user ${useridinput} not found. `
+        result.innerHTML = `user ${useridinput} not found. `;
+        userDetailsContainer.style.display = 'none';
     }
 
 }
